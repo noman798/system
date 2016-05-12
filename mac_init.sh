ac62133a91f4d58bc52639798354a21632dc54ca
noman <man@tz.world>
2016-05-12 11:25:42 +0800

    f

--- mac_init.sh
+++ mac_init.sh
@@ -3,4 +3,4 @@ PREFIX=$(cd "$(dirname "$0")"; pwd)
 ROOT=~/dev
 cp -R $PREFIX/home $ROOT 
 mkdir -p $ROOT/.log $ROOT/.tmp
-docker run -d -v $ROOT:/home/dev -v $ROOT/.log:/var/log -v $ROOT/.tmp:/tmp  --name dev -p 20000:22 -p 20001-20100:20001-20100 -p 8081-8082:8081-8082 80:80 tz/world
+docker run -d -v $ROOT:/home/dev -v $ROOT/.log:/var/log -v $ROOT/.tmp:/tmp  --name dev -p 20000:22 -p 20001-20100:20001-20100 -p 8081-8082:8081-8082 80:80 443:443 tz/world
