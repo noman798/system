3e18f2898fff63d0e95d13309d2682d93bc56784
noman <man@tz.world>
2016-05-12 20:47:30 +0800

    f

--- mnt/home/dev/init.sh
+++ mnt/home/dev/init.sh
@@ -1,10 +1,17 @@
+PREFIX=$(cd "$(dirname "$0")"; pwd)/..
 
+
+cd ~ 
 virtualenv -p /usr/bin/python3 /home/dev/.py3env
+
+mkdir download
+cd download
+
 git clone https://github.com/wting/autojump.git
 cd autojump
 ./install.py
-rm -rf autojump
 source ~/.bashrc
+
 
 
 
