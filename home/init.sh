699cec3f8f8ec33d6bdad89350d091d7c2f153e4
noman <man@tz.world>
2016-05-12 10:53:30 +0800

    f

--- home/init.sh
+++ home/init.sh
@@ -5,10 +5,10 @@ COPY coffeelint.json /home/dev/.coffeelint.json
 COPY hgrc /home/dev/.hgrc
 COPY hgignore /home/dev/.hgignore
 COPY requirement.txt /home/dev/requirement.txt
-COPY bashrc /home/dev/.bashrc
 
 # python libs
 RUN virtualenv -p /usr/bin/python3.5 /home/dev/.py3env
 RUN ["/bin/bash", "-c", "source ~/.py3env/bin/activate && pip3.5 install -e hg+https://btyh17mxy@bitbucket.org/pypa/wheel#egg=python-whell"]
 RUN /bin/bash -c "source ~/.py3env/bin/activate && pip3.5 install -r /home/dev/requirement.txt"
 RUN git clone https://github.com/wting/autojump.git 
+
