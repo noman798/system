5ffe7b8db5026c75790f6f044e6ea6c17d2f8b96
noman <man@tz.world>
2016-05-12 11:06:58 +0800

    f

--- home/init.sh
+++ home/init.sh
@@ -1,7 +1,4 @@
 
-mkdir ~/.hgplugin
-curl https://bitbucket.org/GrahamHelliwell/hg-sync/raw/cdb1d3f0ca68766d775746b02b301547ce3af0df/sync.py > ~/.hgplugin/sync.py
-
 virtualenv -p /usr/bin/python3 /home/dev/.py3env
 git clone https://github.com/wting/autojump.git
 cd autojump
