58f818d5f366dd384be61f89f6ad8cf0d0c56762
noman <man@tz.world>
2016-05-12 11:06:28 +0800

    f

--- home/init.sh
+++ home/init.sh
@@ -2,8 +2,10 @@
 mkdir ~/.hgplugin
 curl https://bitbucket.org/GrahamHelliwell/hg-sync/raw/cdb1d3f0ca68766d775746b02b301547ce3af0df/sync.py > ~/.hgplugin/sync.py
 
-virtualenv -p /usr/bin/python3.5 /home/dev/.py3env
-RUN ["/bin/bash", "-c", "source ~/.py3env/bin/activate && pip3.5 install -e hg+https://btyh17mxy@bitbucket.org/pypa/wheel#egg=python-whell"]
-RUN /bin/bash -c "source ~/.py3env/bin/activate && pip3.5 install -r /home/dev/requirement.txt"
-RUN git clone https://github.com/wting/autojump.git 
+virtualenv -p /usr/bin/python3 /home/dev/.py3env
+git clone https://github.com/wting/autojump.git
+cd autojump
+./install.py
+
+
 
