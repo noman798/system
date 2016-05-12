9ed5597dead12e2a402193ca11ee786be26b5ab2
noman <man@tz.world>
2016-05-12 11:38:15 +0800

    f

--- mac/ssh.sh
+++ mac/ssh.sh
@@ -1,4 +1,7 @@
 docker-machine start
 eval "$(docker-machine env default)"
 docker start dev
-ssh -p20000 dev@`docker-machine ip`
+
+IP=`docker-machine ip`
+echo "DOCKER IP = "$IP
+ssh -p20000 dev@$IP
