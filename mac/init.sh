dad77e8fedd2e2ed956c54e98641a511fcc4825e
noman <man@tz.world>
2016-05-12 19:58:16 +0800

    f

--- mac/init.sh
+++ mac/init.sh
@@ -1,7 +1,26 @@
+docker-machine start
+eval "$(docker-machine env default)"
+
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 $PREFIX/../docker/build.sh
 
-ROOT=~/dev
-cp -R $PREFIX/home $ROOT 
-mkdir -p $ROOT/.log $ROOT/.tmp
-docker run -d -v $ROOT:/home/dev -v $ROOT/.log:/var/log -v $ROOT/.tmp:/tmp  --name dev -p 20000:22 -p 20001-20100:20001-20100 -p 8081-8082:8081-8082 80:80 443:443 tz/world
+BASE=/Users/$USER/dev
+cp -R $PREFIX/home $BASE
+
+ROOT=$BASE/.root
+mkdir -p $ROOT
+cd $ROOT
+mkdir -p var/log \
+    var/lib/redis \
+    var/lib/mongodb \
+    var/lib/mlocate \
+    tmp
+docker run -d -v $ROOT:/home/dev \
+    -v $ROOT/var/log:/var/log \
+    -v $ROOT/tmp:/tmp  \
+    -v $ROOT/var/lib/redis:/var/lib/redis \
+    -v $ROOT/var/lib/mongodb:/var/lib/mongodb \
+    -v $ROOT/var/lib/mlocate:/var/lib/mlocate \
+    --name dev \
+    -p 20000:22 -p 20001-20100:20001-20100 -p 8081-8082:8081-8082 80:80 443:443 \
+    tz/world
