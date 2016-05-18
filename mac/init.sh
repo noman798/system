7aebf4b53ac3472bfb32fb7947355485d9bfa63f
noman_macbook <none@none>
2016-05-18 14:04:03 +0800

    f

--- mac/init.sh
+++ mac/init.sh
@@ -35,6 +35,6 @@ docker run -d -v $ROOT/home:/home/ \
     -v $ROOT/var/lib/mongodb:/var/lib/mongodb \
     -v $ROOT/var/lib/mlocate:/var/lib/mlocate \
     -v $ROOT/data:/data \
-    --name dev \
+    --name tz \
     -p 20000:22 -p 20001-20100:20001-20100 -p 8081-8082:8081-8082 -p 80:80 -p 443:443 \
     tz/world
