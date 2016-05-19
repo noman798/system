acee1e148b96197892a1c37bfa316dc494f5881f
noman <man@tz.world>
2016-05-19 17:25:53 +0800

    f

--- mnt/home/dev/init/project.sh
+++ mnt/home/dev/init/project.sh
@@ -1,5 +1,8 @@
 cd ~
 hg clone ssh://hg@bitbucket.org/vcwatch/tz
+
+pip install -r ~/tz/requirement.txt
+
 hg clone ssh://hg@bitbucket.org/vcwatch/system
 hg clone ssh://hg@bitbucket.org/noman798/room
 
