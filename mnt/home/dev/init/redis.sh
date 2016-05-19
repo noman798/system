7ea432ca5e8bcba48ffb0a783f03374d2ebb33fd
noman <man@tz.world>
2016-05-19 23:59:47 +0800

    f

--- mnt/home/dev/init/redis.sh
+++ mnt/home/dev/init/redis.sh
@@ -1,10 +1,14 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 cd $PREFIX
-wget http://download.redis.io/redis-stable.tar.gz
-tar zxvf redis-stable.tar.gz
-rm redis-stable.tar.gz
-cd redis-stable
-make
-sudo make install
-cd $PREFIX
-rm -rf redis-stable
+
+# wget http://download.redis.io/redis-stable.tar.gz
+# tar zxvf redis-stable.tar.gz
+# rm redis-stable.tar.gz
+# cd redis-stable
+# make
+# sudo make install
+# cd $PREFIX
+# rm -rf redis-stable
+
+mkdir -p /etc/redis
+sudo cp $PREFIX/config/redis*.conf /etc/redis
