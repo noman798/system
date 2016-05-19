0d776811a106add347bc8ceae0bdf9cfd5a10060
noman <man@tz.world>
2016-05-19 19:27:27 +0800

    f

--- mnt/home/dev/init/mongo.sh
+++ mnt/home/dev/init/mongo.sh
@@ -4,6 +4,9 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y --allow-unauthenticated mongodb-org
+sudo rm /etc/mongod.conf
+sudo ln -s $PREFIX/config/mongod.conf /etc/
 
-sudo cp $PREFIX/config/mongod.conf /etc/
+grep "mongodb " /etc/rc.local||sed "$i sudo -u mongodb /usr/bin/mongod  --config /etc/mongod.conf --fork" /etc/rc.local 
+
 
