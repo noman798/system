6967431216072e9bd33511560cf2a51b3bea3c02
noman <man@tz.world>
2016-05-19 17:37:25 +0800

    f

--- mnt/home/dev/init/project.sh
+++ mnt/home/dev/init/project.sh
@@ -1,8 +1,18 @@
 cd ~
+
+curl -fsS https://dlang.org/install.sh | bash -s dmd
+
 hg clone ssh://hg@bitbucket.org/vcwatch/tz
 
 pip install -r ~/tz/requirement.txt
+cd ~/tz/cli/docker
+cp config.example.py config.py 
+python make_config.py
 
+
+
+
+cd ~
 hg clone ssh://hg@bitbucket.org/vcwatch/system
 hg clone ssh://hg@bitbucket.org/noman798/room
 
