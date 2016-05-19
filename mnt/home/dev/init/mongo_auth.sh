7e528abf41c370291363a8254351ed91be70721a
noman <man@tz.world>
2016-05-20 00:57:23 +0800

    f

--- mnt/home/dev/init/mongo_auth.sh
+++ mnt/home/dev/init/mongo_auth.sh
@@ -1,2 +1,6 @@
+# 先修改 /etc/mongo.conf 为 disabled 然后运行这个脚本， 然后再改回去，重启 mongodb
+# security:
+#     # authorization : disabled
+#     authorization : enabled
 
 echo 'db.createUser( { user: "root", pwd: "ce9tLyQdAMhtutBJ", roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] })' | mongo admin --port 20100
