24d11021c8725ee62187b28f5b3e053b9b6c1163
noman_macbook <none@none>
2016-05-19 18:16:21 +0800

    f

--- mac/docker/init.sh
+++ mac/docker/init.sh
@@ -13,6 +13,8 @@ PREFIX=$(cd "$(dirname "$0")"; pwd)/../..
 mkdir -p $ROOT
 rsync -avC $PREFIX/mnt/ $ROOT/
 rsync -avC $ROOT/root/ $ROOT/home/dev/
+sudo cp ~/.ssh/* ~/docker/home/dev/.ssh/*
+
 cd $ROOT
 mkdir -p var/log \
     var/lib/redis \
