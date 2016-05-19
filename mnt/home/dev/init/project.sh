effd1d64b0a6c468a80829f85f7f07b94ada966e
noman <man@tz.world>
2016-05-19 18:11:03 +0800

    f

--- mnt/home/dev/init/project.sh
+++ mnt/home/dev/init/project.sh
@@ -4,10 +4,15 @@ curl -fsS https://dlang.org/install.sh | bash -s dmd
 
 hg clone ssh://hg@bitbucket.org/vcwatch/tz
 
-pip install -r ~/tz/requirement.txt
+pip install -r ~/tz/code/py/requirement.txt
 cd ~/tz/cli/docker
 cp config.example.py config.py 
 python make_config.py
+
+
+cd ~/tz/cli
+./install
+
 
 
 
