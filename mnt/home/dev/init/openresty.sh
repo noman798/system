c2fe64456b5f82954d88e42eeeb34752faa80745
noman <man@tz.world>
2016-05-21 02:26:33 +0800

    f

--- mnt/home/dev/init/openresty.sh
+++ mnt/home/dev/init/openresty.sh
@@ -5,26 +5,25 @@ cd /tmp
 rm openresty.zip
 rm -rf openresty-*
 
-wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -O openresty.zip -c
-unzip openresty.zip
-rm openresty.zip
-cd openresty-*/
-make try-luajit
-cd openresty-*/
-./configure --with-luajit
-make && sudo make install
+# wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -O openresty.zip -c
+# unzip openresty.zip
+# rm openresty.zip
+# cd openresty-*/
+# make try-luajit
+# cd openresty-*/
+# ./configure --with-luajit
+# make && sudo make install
+
 sudo useradd nginx
-
-
 sudo mkdir -p /var/log/nginx /etc/nginx/conf.d/
 sudo chown -R nginx:nginx /var/log/nginx /etc/nginx
 
-cp $PREFIX/config/nginx.conf /etc/nginx/
-rm /usr/local/openresty/nginx/conf/nginx.conf
-ln -s /etc/nginx/nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+sudo cp $PREFIX/config/nginx.conf /etc/nginx/
+sudo rm /usr/local/openresty/nginx/conf/nginx.conf
+sudo ln -s /etc/nginx/nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
 
 cd $PREFIX
-rm -rf openresty-
+sudo rm -rf openresty-
 
 
 sudo ln -s $PREFIX/supervisord/nginx.conf /etc/supervisor/conf.d/
