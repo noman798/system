7628bc4d8ca7f667a0c5cdb6dbd22cd46cc0e38f
noman <man@tz.world>
2016-05-21 02:04:42 +0800

    f

--- mnt/home/dev/init/openresty.sh
+++ mnt/home/dev/init/openresty.sh
@@ -1,6 +1,9 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 cd /tmp
 
+rm openresty.zip
+rm -rf openresty-
+
 wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -O openresty.zip -c
 unzip openresty.zip
 rm openresty.zip
