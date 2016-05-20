776f31ad51d065d2ebce5904148b455641d37cb5
noman <man@tz.world>
2016-05-21 01:20:25 +0800

    f

--- mnt/home/dev/init/openresty.sh
+++ mnt/home/dev/init/openresty.sh
@@ -1,6 +1,8 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 cd $PREFIX
 
+wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -o openresty.zip
+
 # wget http://download.redis.io/redis-stable.tar.gz
 # tar zxvf redis-stable.tar.gz
 # rm redis-stable.tar.gz
@@ -10,11 +12,3 @@ cd $PREFIX
 # cd $PREFIX
 # rm -rf redis-stable
 
-sudo useradd redis
-sudo chown redis /var/lib/redis/
-mkdir -p /etc/redis
-sudo cp $PREFIX/config/redis*.conf /etc/redis
-
-sudo ln -s $PREFIX/supervisord/redis*.conf /etc/supervisor/conf.d/
-sudo supervisorctl reload 
-sudo supervisorctl restart all
