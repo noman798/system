6f7e99d6391b83caf7788d1d27eaeda64522729a
noman <man@tz.world>
2016-05-21 01:32:12 +0800

    f

--- mnt/home/dev/init/openresty.sh
+++ mnt/home/dev/init/openresty.sh
@@ -1,11 +1,12 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 cd $PREFIX
 
-wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -o openresty.zip
+wget `curl -s https://api.github.com/repos/openresty/openresty/tags | grep zipball_url | head -n 1 | cut -d '"' -f 4` -O openresty.zip -c
+unzip openresty.zip
+rm openresty.zip
+cd openresty-*/
+make
 
-# wget http://download.redis.io/redis-stable.tar.gz
-# tar zxvf redis-stable.tar.gz
-# rm redis-stable.tar.gz
 # cd redis-stable
 # make
 # sudo make install
