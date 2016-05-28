1ef09410ee9c46010999bccd59d93e0582cf3908
noman <man@tz.world>
2016-05-28 23:28:52 +0800

    f

--- linux.sh
+++ linux.sh
@@ -15,3 +15,17 @@ mkdir -p var/log \
     data \
     tmp
 sudo chown -R 1000 $ROOT
+
+docker pull daocloud.io/zuroc/tz:master-bcd02ab
+
+docker run -d -v $ROOT/home:/home \
+    -v $ROOT/var/log:/var/log \
+    -v $ROOT/tmp:/tmp  \
+    -v $ROOT/root:/root  \
+    -v $ROOT/var/lib/redis:/var/lib/redis \
+    -v $ROOT/var/lib/mongodb:/var/lib/mongodb \
+    -v $ROOT/var/lib/mlocate:/var/lib/mlocate \
+    -v $ROOT/data:/data \
+    --name tzol \
+    -p 30000:22 -p 30001-30100:30001-30100 -p 8081-8082:30101-30102 -p 80:30103 -p 443:30104 \
+    daocloud.io/zuroc/tz
