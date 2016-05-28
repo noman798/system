6dbab2d9900a432f66dcc684e2275bb53806de3a
noman <man@tz.world>
2016-05-29 00:17:34 +0800

    f

--- mnt/home/dev/init/project.sh
+++ mnt/home/dev/init/project.sh
@@ -2,7 +2,7 @@ cd ~
 
 sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
 wget -qO - http://dlang.org/d-keyring.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install dmd-bin -y
+sudo apt-get update && sudo apt-get install dmd-bin -y  --allow-unauthenticated
 
 
 hg clone ssh://hg@bitbucket.org/vcwatch/tz
