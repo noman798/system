37cfa3ba7625ee2a3158edf6fec206be7cce8c89
noman <man@tz.world>
2016-06-05 13:09:27 +0800

    f

--- mnt/home/dev/init/search.sh
+++ mnt/home/dev/init/search.sh
@@ -5,10 +5,19 @@ sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-sudo apt-get install oracle-java9-installer -y
+sudo apt-get install oracle-java8-installer maven -y
 
 wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo echo "deb http://packages.elasticsearch.org/elasticsearch/1.1/debian stable main" >> /etc/apt/sources.list
 
 sudo apt-get update -y
 sudo apt-get install elasticsearch -y
+mkdir -p ~/download
+cd ~/download
+
+git clone https://github.com/huaban/elasticsearch-analysis-jieba
+cd elasticsearch-analysis-jieba
+mvn package
+/usr/share/elasticsearch/plugins
+
+
