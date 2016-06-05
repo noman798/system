fd316fee3eda004e254af79e89c7c1fa7577fc1e
noman <man@tz.world>
2016-06-05 19:56:55 +0800

    f

--- mnt/home/dev/init/search.sh
+++ mnt/home/dev/init/search.sh
@@ -1,23 +1,26 @@
-sudo apt-get update
-sudo apt-get upgrade
+PREFIX=$(cd "$(dirname "$0")"; pwd)
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install oracle-java8-installer maven -y
 
-wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo echo "deb http://packages.elasticsearch.org/elasticsearch/1.1/debian stable main" >> /etc/apt/sources.list
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+
 
 sudo apt-get update -y
 sudo apt-get install elasticsearch -y
 mkdir -p ~/download
 cd ~/download
 
-git clone https://github.com/huaban/elasticsearch-analysis-jieba
-cd elasticsearch-analysis-jieba
-mvn package
-/usr/share/elasticsearch/plugins
+
+wget https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v1.9.3/elasticsearch-analysis-ik-1.9.3.zip
+
+sudo bin/plugin install file:~/download/elasticsearch-analysis-ik-1.9.3.zip
 
 
